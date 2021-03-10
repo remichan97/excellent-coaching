a57327f13da6fef3079df40b570187f9098625fb
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-03-10 21:13:00 +0700

    trang about us, sửa lại pattern prg của trang đăng ký khóa học

--- layout/footer.php
+++ layout/footer.php
@@ -2,7 +2,7 @@
 	<div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
 	<ul class="list-inline">
 		<li class="list-inline-item"><a href="../">Trang chính</a></li>
-		<li class="list-inline-item"><a href="?request=about">Giới thiệu về trung tâm</a></li>
+		<li class="list-inline-item"><a href="../about">Giới thiệu về trung tâm</a></li>
 		<li class="list-inline-item"><a href="?request=people">Giảng viên</a></li>
 		<li class="list-inline-item"><a href="../?request=contact">Liên hệ với chúng tôi</a></li>
 	</ul>
