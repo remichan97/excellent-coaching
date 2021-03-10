514bdce5cf312236d2442c9697b2a9805cb0b4e3
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-03-10 13:45:40 +0700

    tách trang hiển thị thông tin khóa ra khỏi index

---
+++ courses/index.php
@@ -0,0 +1,65 @@
+<?php include "../model/database.php"?>
+<?php include "../controller/showinfo.php" ?>
+<?php include "../view/registerstatus.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<title>site</title>
+	<link rel="stylesheet" href="../assets/subsite/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/subsite/fonts/ionicons.min.css">
+	<link rel="stylesheet" href="../assets/subsite/css/Contact-Form-Clean.css">
+	<link rel="stylesheet" href="../assets/subsite/css/Footer-Basic.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+	<link rel="stylesheet" href="../assets/subsite/css/logo_tag1.css">
+	<link rel="stylesheet" href="../assets/subsite/css/styles.css">
+</head>
+
+<body>
+	<?php include "../layout/header.php"?>
+	</div>
+	<section class="content_academy_wrap">
+		<div class="content_academy_inner">
+			<header class="title_form">
+				<div class="col_9 layer">
+					<h1>Heading</h1>
+				</div>
+				<div class="col_3">
+					<h4>Thông tin khóa học</h4>
+				</div>
+				<section class="description_form">
+					<div class="col_9">
+						<p>It's not only writers who can benefit from this free online tool. If you're a programmer who's working on a project where blocks of text are needed, this tool can be a great way to get that. It's a good way to test your programming and that the tool being created is working well.<br></p>
+						<p>Slave spirituals often had hidden double meanings. On one level, spirituals referenced heaven, Jesus, and the soul, but on another level, the songs spoke about slave resistance. For example, according to Frederick Douglass, the song “O Canaan, Sweet Canaan” spoke of slaves’ longing for heaven, but it also expressed their desire to escape to the North. Careful listeners heard this second meaning in the following lyrics: “I don’t expect to stay / Much longer here. / Run to Jesus, shun the danger. / I don’t expect to stay.” When slaves sang this song, they could have been speaking of their departure from this life and their arrival in heaven; however, they also could have been describing their plans to leave the South and run, not to Jesus, but to the North. Slaves even used songs like “Steal Away to Jesus (at midnight)” to announce to other slaves the time and place of secret, forbidden meetings. What whites heard as merely spiritual songs, slaves discerned as detailed messages. The hidden meanings in spirituals allowed slaves to sing what they could not say.<br></p>
+					</div>
+					<div class="col_3">
+						<div class="list_lecturers">
+							<ul class="top_list">
+								<li>Item 1</li>
+								<li>Item 2</li>
+								<li>Item 3</li>
+								<li>Item 4</li>
+							</ul>
+						</div>
+					</div>
+				</section>
+			</header>
+		</div>
+	</section>
+	<section class="content_empty_wrap">
+		<div class="content_academy_inner">
+			<section class="contact-clean">
+				<?php include "../view/registerform.php"?>
+			</section>
+		</div>
+		<?php include "../layout/footer.php" ?>
+	</section>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/bs-init.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+</body>
+
+</html>
--- view/registerform.php
+++ view/registerform.php
@@ -1,41 +1,45 @@
-<?php include "./controller/courseregister.php"?>
+<?php include "../controller/courseregister.php" ?>
 
 <div class="register">
 	<h4>Bạn sẽ tham gia khóa học này!</h4>
 	<p>Vui lòng dành chút thời gian để điền một số thông tin dưới đây, và chúng tôi sẽ thông báo cho bạn thời gian cụ thể bạn cần đến trung tâm để hoàn tất hồ sơ đăng ký</p>
-	<form method="POST">
-		<div class="form-row">
-			<div class="form-group col-md-4">
-				<label for="name">Họ và tên:</label><input type="text" name='name' id="name" class="form-control" required>
-			</div>
-			<div class="form-group col-md-4">
-				<label for="email">Email:</label><input type="text" id="email" class="form-control" name='email' required>
-			</div>
-			<div class="form-group col-md-4">
-				<label for="phone">Số điện thoại:</label><input type="tel" id="phone" class="form-control" name='phone' pattern="^0[0-9]+" required>
-			</div>
-			<div class="form-group col-md-12">
-				<label for="home">Địa chỉ:</label><input type="text" name='address' id="home" class="form-control" required>
-			</div>
-			<div class="form-group col-md-12">
-				<label for="address">Bạn sẽ tham gia khóa học này ở cơ sở: </label>
-				<select id="addresss" name='centre' class="form-control" required>
-					<option value="" hidden>Chọn một cơ sở...</option>
-					<?php foreach($a as $i):?>
-					<option value="<?=$i['addID']?>"><?=$i['address']?></option>
-					<?php endforeach;?>
-				</select>
-			</div>
-			<div class="form-group col-md">
-				<label for="ref">Bạn biết đến chúng tôi thông qua kênh truyền thông nào:</label>
-				<select id="ref" class="form-control" name="ref" required>
-					<option hidden>Chọn một hình thức...</option>
-					<option value="Facebook">Facebook</option>
-					<option value="Google">Tìm kiếm trên Google</option>
-					<option value="Khác">Khác</option>
-				</select>
-			</div>
+	<form method="POST" class="row g-3">
+		<div class="col-md-4">
+			<label for="name" class="form-label">Họ và tên</label>
+			<input type="text" name="name" id="name" class="form-control" required="required">
 		</div>
-		<button type="submit" class="btn btn-primary" name="submit">Gửi đăng ký</button>
+		<div class="col-md-4">
+			<label for="email" class="form-label">Địa chỉ Email</label>
+			<input type="email" name="email" id="email" class="form-control" required="required">
+		</div>
+		<div class="col-md-4">
+			<label for="name" class="form-label">Số điện thoại</label>
+			<input type="text" name="phone" id="phone" class="form-control" required="required" pattern="0[0-9]+">
+		</div>
+		<div class="col-12">
+			<label for="address" class="form-label">Địa chỉ: </label>
+			<input type="text" name="a" id="address" class="form-control" required>
+		</div>
+		<div class="col-12">
+			<label for="centre" class="form-label">Bạn sẽ tham gia khóa học tại cơ sở nào?</label>
+			<select name="centre" id="centre" class="form-control" required>
+				<option value="" hidden>Chọn một cơ sở...</option>
+				<?php foreach($a as $cen):?>
+					<option value="Cơ sở <?=$cen['addID']?>: <?=$cen['address']?>"><?=$cen['address']?></option>
+				<?php endforeach;?>
+			</select>
+		</div>
+		<div class="col-12">
+			<label for="ref" class="form-label">Bạn biết đến chúng tôi thông qua kênh truyền thông nào?</label>
+			<select name="ref" id="ref" required class="form-control">
+				<option value="" hidden>Chọn một kênh truyền thông...</option>
+				<option value="Facebook">Facebook</option>
+				<option value="Google">Tìm kiếm trên Google</option>
+				<option value="Khác">Khác</option>
+			</select>
+		</div>
+		<div class="col-12">
+			<button type="submit" class="btn btn-primary">Gửi đăng ký</button>
+		</div>
 	</form>
 </div>
