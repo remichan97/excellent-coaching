1aa171465900ae93c5bfa4877ee9fe393e1ceb02
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-03-16 17:18:49 +0700

    thay đổi cỡ chữ hiển thị

--- about/index.php
+++ about/index.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="../assets/subsite/css/Contact-Form-Clean.css">
     <link rel="stylesheet" href="../assets/subsite/css/Footer-Basic.css">
     <link rel="stylesheet" href="../assets/subsite/css/logo_tag1.css">
-    <link rel="stylesheet" href="../assets/subsite/css/styles.css">
 </head>
 
 <body>
@@ -24,20 +23,20 @@
                     </div>
                     <section class="description_form">
                         <div class="col_9">
-                            <h4>Excellent Coaching Ấn Độ</h4>
+                            <h3>Excellent Coaching Ấn Độ</h3>
                             <p>Excellent Coaching là tổ chức đào tạo được thành lập vào năm 2001 tại Dwarka, Ấn Độ. Excellent Coaching đã có hơn 20 năm kinh nghiệm đào tạo Công nghệ Thông tin &#40;CNTT&#41; tại Ấn Độ, và hàng triệu lượt học viên tốt nghiệp và thành công trên con đường họ đã chọn. Sứ mệnh và mục tiêu của Excellent Coaching là cung cấp một chương trình đào tạo chất lượng với các chương trình học chú trọng vào thực hành nhằm đảm bảo các học viên có thể phát huy tối đa khả năng của họ trên cong đường sự nghiệp đã chọn.<br /></p>
-                            <h4>Excellent Coaching Vietnam</h4>
+                            <h3>Excellent Coaching Vietnam</h3>
                             <p>Với mong muốn tiếp cận với các học viên quốc tế, Excellent Coaching đã thành lập cơ sở đầu tiên tại Việt Nam vào năm 2010. Sau hơn 10 năm hoạt động, chúng tôi đã đào tạo được hơn 100.000 Lập trình viên Quốc Tế cho ngành CNTT tại Việt Nam. <a href="../">Các khóa học</a> của Excellent Coaching Vietnam được thiết kế phù hợp với tiêu chuẩn Quốc Tế, giúp cho học viên có cơ hội tiếp thu, lĩnh hội, học hỏi lượng kiến thức chất lượng về công nghệ được công công nhận và áp dụng tại các quốc gia trên thế giới.<br /></p>
-                            <h4>Sứ mệnh của Excellent Coaching Vietnam</h4>
+                            <h3>Sứ mệnh của Excellent Coaching Vietnam</h3>
                             <p>CNTT luôn là lĩnh vực nhiều người chọn học, và chúng tôi luôn tư hào về chất lượng các khóa học, cũng như chất lượng giảng dạy của <a href="../teacher">đội ngũ giảng viên</a> của chúng tôi. Không ngừng nâng cao chất lượng khóa học cũng như chất lượng giảng dạy, chúng tôi hy vọng sẽ đáp ứng được nhu cầu học ngày càng cao của học viên, cũng như những sinh viên, học sinh có nguyện vọng tham gia vào ngành CNTT trong tương lai.</p>
-                            <h4>Các cơ sở của Excellent Coaching Vietnam</h4>
+                            <h3>Các cơ sở của Excellent Coaching Vietnam</h3>
                             <p>Hiện tại Excellent Coaching đang có các trụ sở sau: </p>
-                            <h5>Excellent Coaching Vietnam - Cơ sở 1:</h5>
+                            <h4>Excellent Coaching Vietnam - Cơ sở 1:</h4>
                             <ul>
                                 <li>Địa chỉ: số 54 Lê Thanh Nghị - phường Bách Khoa - quận Hai Bà Trưng - Hà Nội</li>
                                 <li>Vị trí trên bản đồ: <a href="https://www.google.com/maps/place/54+L%C3%AA+Thanh+Ngh%E1%BB%8B,+B%C3%A1ch+Khoa,+Hai+B%C3%A0+Tr%C6%B0ng,+H%C3%A0+N%E1%BB%99i,+Vietnam/@21.0032683,105.8472392,17z/data=!3m1!4b1!4m5!3m4!1s0x3135ad58455db2ab:0x9b3550bc22fd8bb!8m2!3d21.0032633!4d105.8494279">Xem trên bản đồ</a></li>
                             </ul>
-                            <h5>Excellent Coaching Vietnam - Cơ sở 2:</h5>
+                            <h4>Excellent Coaching Vietnam - Cơ sở 2:</h4>
                             <ul>
                                 <li>Địa chỉ: số 285 Đội Cấn - phường Vĩnh Phúc - quận Ba Đình - Hà Nội</li>
                                 <li>Vị trí trên bản đồ: <a href="https://www.google.com/maps/place/285+%C4%90%E1%BB%99i+C%E1%BA%A5n,+V%C4%A9nh+Ph%C3%BA,+Ba+%C4%90%C3%ACnh,+H%C3%A0+N%E1%BB%99i+100000,+Vietnam/@21.0357156,105.8168405,17z/data=!3m1!4b1!4m5!3m4!1s0x3135ab145bf89bd7:0xd94a869b494c04b6!8m2!3d21.0357106!4d105.8190292">Xem trên bản đồ</a></li>
