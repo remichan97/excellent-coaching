aae8add5ab34d022849e824b71c11a8a04ac3fa7
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-03-16 22:44:01 +0700

    CSDL mới

--- database/excoaching.sql
+++ database/excoaching.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2021 at 01:36 PM
+-- Generation Time: Mar 16, 2021 at 04:34 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -52,6 +52,13 @@ CREATE TABLE `admin` (
   `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '96549e1e8ed56cbc3488058e98a5c3bc');
+
 -- --------------------------------------------------------
 
 --
@@ -70,7 +77,6 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`catID`, `catName`) VALUES
 (1, 'Art'),
 (2, 'Commerce'),
-(5, 'Game Design'),
 (4, 'Language'),
 (3, 'Mathematics');
 
@@ -96,10 +102,10 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`cID`, `catID`, `cName`, `teacher`, `price`, `description`, `duration`, `status`) VALUES
-(1, 1, '3D Art', 3, 9900000, 'Any information here', 5, b'0'),
+(1, 1, '3D Art', 3, 9900000, '<p>Any information here</p>\r\n', 5, b'1'),
 (2, 1, 'Animation', 3, 15000000, 'Whatever here', 1, b'1'),
 (3, 4, 'English', 1, 8000000, '<h1>Thông tin khóa học</h1>\r\n\r\n<p>Khóa học tiếng Anh của chúng tôi sẽ giúp các bạn có thể dễ dàng tiếp cận với người nước ngoài hơn trong các cuộc đối thoại!</p>', 3, b'1'),
-(4, 3, 'Cost Accountant', 2, 20000000, '', 2, b'0');
+(4, 3, 'Cost Accountant', 2, 20000000, '', 2, b'1');
 
 -- --------------------------------------------------------
 
@@ -111,15 +117,17 @@ CREATE TABLE `feedback` (
   `id` int(4) NOT NULL,
   `email` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `subject` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `body` longtext COLLATE utf8_unicode_ci NOT NULL
+  `body` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `response` bit(1) NOT NULL DEFAULT b'0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `feedback`
 --
 
-INSERT INTO `feedback` (`id`, `email`, `subject`, `body`) VALUES
-(1, 'binh.truongvu97@outlook.com', 'Phản hồi đầu tiên', 'Hi,\r\nĐây là phản hồi đầu tiên của trung tâm!');
+INSERT INTO `feedback` (`id`, `email`, `subject`, `body`, `response`) VALUES
+(1, 'binh.truongvu97@outlook.com', 'Phản hồi đầu tiên', 'Hi,\r\nĐây là phản hồi đầu tiên của trung tâm!', b'1'),
+(2, 'binh.truongvu97@outlook.com', 'Phản hồi thứ hai', 'Hi,\r\nĐây là phản hồi thứ hai', b'1');
 
 -- --------------------------------------------------------
 
@@ -128,12 +136,13 @@ INSERT INTO `feedback` (`id`, `email`, `subject`, `body`) VALUES
 --
 
 CREATE TABLE `register` (
-  `id` int(5) NOT NULL,
+  `id` int(3) NOT NULL,
   `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `cID` int(2) NOT NULL,
-  `addID` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `ref` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `cID` int(3) NOT NULL,
+  `addID` int(2) NOT NULL,
+  `ref` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `regDate` date NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -141,9 +150,8 @@ CREATE TABLE `register` (
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`id`, `name`, `email`, `cID`, `addID`, `ref`, `date`) VALUES
-(1, 'Trương Vũ Bình', 'binh.truongvu97@outlook.com', 3, 'Cơ sở 1: 285 Đội Cấn, Liễu Giai, Ba Đình, Hanoi', 'Facebook', '0000-00-00'),
-(2, 'Trương Vũ Bình', 'binh.truongvu97@gmail.com', 3, 'Cơ sở 1: 285 Đội Cấn, Liễu Giai, Ba Đình, Hanoi', 'Google', '0000-00-00');
+INSERT INTO `register` (`id`, `name`, `email`, `cID`, `addID`, `ref`, `regDate`, `date`) VALUES
+(1, 'Trương Vũ Bình', 'binh.truongvu97@gmail.com', 3, 1, 'Facebook', '2021-03-12', '2021-04-16');
 
 -- --------------------------------------------------------
 
@@ -154,6 +162,7 @@ INSERT INTO `register` (`id`, `name`, `email`, `cID`, `addID`, `ref`, `date`) VA
 CREATE TABLE `teacher` (
   `id` int(11) NOT NULL,
   `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `profession` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `photo` text COLLATE utf8_unicode_ci NOT NULL,
   `description` longtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -162,10 +171,10 @@ CREATE TABLE `teacher` (
 -- Dumping data for table `teacher`
 --
 
-INSERT INTO `teacher` (`id`, `name`, `photo`, `description`) VALUES
-(1, 'Truong Vu Binh', '', 'Giang vien A'),
-(2, 'Dang Tran Long', '', 'Giao vien B'),
-(3, 'Dao Ngoc Quan', '', 'Giang vien C');
+INSERT INTO `teacher` (`id`, `name`, `profession`, `photo`, `description`) VALUES
+(1, 'Trương Vũ Bình', 'PHP, HTML', '1615645051_81783070_p0.jpg', '<p>Experience</p>\r\n'),
+(2, 'Dang Tran Long', 'HTML, PHP, CSS, Python', 'Epyx6SbUUAE01Ft.jpg', 'Giao vien B'),
+(3, 'Dao Ngoc Quan', 'HTML, CSS', 'EqoXy3dVEAEeBkc.jpg', 'Giang vien C');
 
 --
 -- Indexes for dumped tables
@@ -209,9 +218,7 @@ ALTER TABLE `feedback`
 -- Indexes for table `register`
 --
 ALTER TABLE `register`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `address` (`addID`),
-  ADD KEY `course` (`cID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `teacher`
@@ -233,37 +240,37 @@ ALTER TABLE `address`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `catID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `catID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `cID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `cID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `teacher`
 --
 ALTER TABLE `teacher`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
