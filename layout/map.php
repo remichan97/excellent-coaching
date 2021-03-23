1bac2e19887b1e1a7d16276835d8eaa166838752
quan <quancaps@gmail.com>
2021-03-24 01:47:29 +0700

    sửa map

--- layout/map.php
+++ layout/map.php
@@ -1,14 +1,10 @@
 <section class="content_academy_wrap">
-	<div class="content_academy_inner">
-		<div class="col_6">
-			<div class="box_layout">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2214.7438101902917!2d105.84893673507364!3d21.003046368878216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad58455db2ab%3A0x9b3550bc22fd8bb!2zNTQgTMOqIFRoYW5oIE5naOG7iywgQsOhY2ggS2hvYSwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1615366089109!5m2!1svi!2s" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-			</div>
+	<div class="content_academy_inner" style="text-align: center;">
+		<div class="map-wrap">
+			<iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8780333429204!2d105.80934496356527!3d21.03756565034616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab145bf89bd7%3A0xd9c72ba66f9c3d09!2zMjg1IMSQ4buZaSBD4bqlbiwgVsSpbmggUGjDuiwgQmEgxJDDrG5oLCBIw6AgTuG7mWkgMTAwMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1616498683727!5m2!1svi!2s" width="300" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 		</div>
-		<div class="col_6">
-			<div class="box_layout">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.92312385199!2d105.81679641540238!3d21.035761792913956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0d127a01e7%3A0xab069cd4eaa76ff2!2zMjg1IMSQ4buZaSBD4bqlbiwgVsSpbmggUGjDuiwgQmEgxJDDrG5oLCBIw6AgTuG7mWkgMTAwMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1615366856638!5m2!1svi!2s" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-			</div>
+		<div class="map-wrap">
+			<iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.734722425265!2d105.84723921540208!3d21.00326829402276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad58455db2ab%3A0x9b3550bc22fd8bb!2zNTQgTMOqIFRoYW5oIE5naOG7iywgQsOhY2ggS2hvYSwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1616498750148!5m2!1svi!2s" width="300" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 		</div>	
 	</div>
 </section>
