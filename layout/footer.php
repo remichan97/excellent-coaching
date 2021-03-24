8cd762a37bbd87569cb7b4afa7b441425d5330a1
quang045 <dinhsyquangkbc@gmail.com>
2021-03-24 12:51:32 +0700

    Tăng độ đậm cho thẻ a footer

--- layout/footer.php
+++ layout/footer.php
@@ -1,11 +1,18 @@
 
 <footer class="footer-basic">
-	<div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+	<div class="social">
+		<a href="#"><i class="icon ion-social-instagram" style="color: blue;"></i></a>
+		<a href="#"><i class="icon ion-social-snapchat" style="color: blue;"></i></a>
+		<a href="#"><i class="icon ion-social-twitter" style="color: blue;"></i></a>
+		<a href="#"><i class="icon ion-social-facebook" style="color: blue;"></i></a>
+	</div>
+
 	<ul class="list-inline">
-		<li class="list-inline-item"><a href="../">Trang chính</a></li>
-		<li class="list-inline-item"><a href="../about">Giới thiệu về trung tâm</a></li>
-		<li class="list-inline-item"><a href="../teacher">Giảng viên</a></li>
-		<li class="list-inline-item"><a href="../?request=contact">Liên hệ với chúng tôi</a></li>
+		<li class="list-inline-item"><a href="../" style="font-weight:bold;">Trang chính</a></li>
+		<li class="list-inline-item"><a href="../about" style="font-weight:bold;">Giới thiệu về trung tâm</a></li>
+		<li class="list-inline-item"><a href="../teacher" style="font-weight:bold;">Giảng viên</a></li>
+		<li class="list-inline-item"><a href="../?request=contact" style="font-weight:bold;">Liên hệ với chúng tôi</a></li>
 	</ul>
-	<p class="copyright">Excellent Coaching Vietnam © 2021</p>
+	<br>
+	<p class="copyright" style="font-weight: bold;">Excellent Coaching Vietnam © 2021</p>
 </footer>
