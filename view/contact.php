70f1c59957bdc44d5729476519f9b8afac934e3d
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-03-28 21:43:21 +0700

    sử dụng validate của bootstrap vào website thay cho validate truyền thống của HTML5

--- view/contact.php
+++ view/contact.php
@@ -8,19 +8,22 @@
 			<?php include "./view/contactstatus.php"?>
 			<h2>Bạn đang liên hệ với chúng tôi!</h2>
 			<p>Bạn có thắc mắc cần được giải đáp, hoặc có đề xuất cho chúng tôi? Hãy cho chúng tôi biết ý kiến của bạn thông qua biểu mẫu dưới đây!</p>
-			<form method="POST">
+			<form method="POST" class="needs-validation" novalidate>
 				<div class="form-row">
 					<div class="form-group col-md-6">
 					<label for="email">Email của bạn:</label>
 					<input type="text" id="email" name="email" required="required" class="form-control" required>
+					<div class="invalid-feedback">Vui lòng điền email của bạn</div>
 					</div>
 					<div class="form-group col-md-6">
 					<label for="subject">Tiêu đề:</label>
 					<input type="text" id="subject" name="subject" required="required" class="form-control" required>
+					<div class="invalid-feedback">Vui lòng điền tiêu đề tin nhắn</div>
 					</div>
 					<div class="form-group col-md">
 					<label for="body">Nội dung tin nhắn:</label>
 					<textarea name="body" id="body" class="form-control" cols="30" rows="10" required></textarea>
+					<div class="invalid-feedback">Vui lòng điền nội dung tin nhắn</div>
 					</div>
 				</div>
 				<button type="submit" class="btn btn-primary" name='sendmsg'>Gửi phản hồi</button>
@@ -28,3 +31,24 @@
 		</div>
 	</div>
 </div>
+
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
--- view/registerform.php
+++ view/registerform.php
@@ -3,22 +3,26 @@
 <div class="register">
 	<h4>Bạn sẽ tham gia khóa học này!</h4>
 	<p>Vui lòng dành chút thời gian để điền một số thông tin dưới đây, và chúng tôi sẽ thông báo cho bạn thời gian cụ thể bạn cần đến trung tâm để hoàn tất hồ sơ đăng ký</p>
-	<form method="POST" class="row g-3">
+	<form method="POST" class="row g-3 needs-validation" novalidate>
 		<div class="col-md-4">
 			<label for="name" class="form-label">Họ và tên</label>
 			<input type="text" name="name" id="name" class="form-control" required="required">
+			<div class="invalid-feedback">Thông tin bắt buộc</div>
 		</div>
 		<div class="col-md-4">
 			<label for="email" class="form-label">Địa chỉ Email</label>
 			<input type="email" name="email" id="email" class="form-control" required="required">
+			<div class="invalid-feedback">Thông tin bắt buộc</div>
 		</div>
 		<div class="col-md-4">
 			<label for="name" class="form-label">Số điện thoại</label>
 			<input type="text" name="phone" id="phone" class="form-control" required="required" pattern="0[0-9]+" maxlength="11">
+			<div class="invalid-feedback">Thông tin bắt buộc</div>
 		</div>
 		<div class="col-12">
 			<label for="address" class="form-label">Địa chỉ: </label>
 			<input type="text" name="address" id="address" class="form-control" required>
+			<div class="invalid-feedback">Thông tin bắt buộc</div>
 		</div>
 		<div class="col-12">
 			<label for="centre" class="form-label">Bạn sẽ tham gia khóa học tại cơ sở nào?</label>
@@ -28,6 +32,7 @@
 					<option value="<?=$cen['addID']?>"><?=$cen['address']?></option>
 				<?php endforeach;?>
 			</select>
+			<div class="invalid-feedback">Vui lòng chọn một trung tâm bạn muốn tham gia khóa học</div>
 		</div>
 		<div class="col-12">
 			<label for="ref" class="form-label">Bạn biết đến chúng tôi thông qua kênh truyền thông nào?</label>
@@ -37,9 +42,31 @@
 				<option value="Google">Tìm kiếm trên Google</option>
 				<option value="Kênh truyền thông khác">Khác</option>
 			</select>
+			<div class="invalid-feedback">Vui lòng chọn một kênh truyền thông</div>
 		</div>
 		<div class="col-12">
 			<button type="submit" class="btn btn-primary" name="submit">Gửi đăng ký</button>
 		</div>
 	</form>
 </div>
+
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
